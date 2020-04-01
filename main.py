a3bfd846193db1a2149b818ea59deb0a7903b0c0
YafuR <975341793@qq.com>
2020-03-31 22:29:38 -0400

    auto login

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import os
 import random
+import time
+
 import bs4
 from selenium import webdriver
-import os
-import time
+import util
+
+auto_log = 0
+
 
 def freshkiller_s(url):
     driver = webdriver.Chrome(executable_path='./chromedriver')
     driver.get(url)
     soup = bs4.BeautifulSoup(driver.page_source,'html.parser')
-    time.sleep(45)
+    if not auto_log:
+        time.sleep(45)
+    else:
+        util.auto_login(driver)
     #add auto login
     no_slots = True
     n = 0
@@ -32,9 +40,9 @@ def freshkiller_s(url):
             for i in range(10):
                 os.system('say "Slots Founded"')
             time.sleep(3600)
-        time.sleep(random.randint(1,5))
+        time.sleep(random.randint(1,10))
         print(str(n) + "st try, run time:" + str(time.time() - start_time))
 
 if __name__ == '__main__':
-    url = "https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1"
+    url = "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fcart%2Fview.html%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
     freshkiller_s(url)
--- util.py
+++ util.py
@@ -1,2 +1,25 @@
+import time
+import random
 def auto_login(driver):
-    f = open(login_info.txt)
+    f = open('login_info.txt')
+    info = []
+    for item in f:
+        info.append((item))
+    email = driver.find_element_by_xpath("//*[@id=\"ap_email\"]")
+    time.sleep(random.randint(1, 3))
+    email.send_keys(info[0])
+    time.sleep(random.randint(1,3))
+    password = driver.find_element_by_xpath("//*[@id=\"ap_password\"]")
+    password.send_keys(info[1])
+    time.sleep(random.randint(1, 3))
+    login_button = driver.find_element_by_xpath("//*[@id=\"signInSubmit\"]")
+    login_button.click()
+    time.sleep(random.randint(1, 3))
+    checkout_button = driver.find_element_by_xpath("//*[@id=\"sc-alm-buy-box-ptc-button-QW1hem9uIEZyZXNo\"]/span/input")
+    checkout_button.click()
+    time.sleep(random.randint(1, 2))
+    continue_button = driver.find_element_by_xpath("//*[@id=\"a-autoid-0\"]")
+    continue_button.click()
+    #time.sleep(1000)
+
+
